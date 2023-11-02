7105fb1ae991217ea39df4fda891d73f44270cbb
manbir1298 <manbirdeol123@gmail.com>
2023-11-03 01:33:34 +0530

    Added File

---
+++ B.java
@@ -0,0 +1,16 @@
+package p2;
+
+public class B
+{
+public int x=10;
+
+public void show()
+{
+System.out.println("Hello World");
+}
+
+public static void main(String[] args)
+{
+System.out.println("Class B");
+}
+} 
